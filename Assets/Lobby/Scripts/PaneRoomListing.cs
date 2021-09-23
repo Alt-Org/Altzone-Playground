0502aaa7618fb3676ca295f0fc76275065286220
Jari Petays <jari.petays@iki.fi>
2021-09-23 14:03:27 +0300

    list rooms in lobby v1 might work

--- Assets/Lobby/Scripts/PaneRoomListing.cs
+++ Assets/Lobby/Scripts/PaneRoomListing.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Photon.Pun;
+using Prg.Scripts.Common.Photon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,24 +10,71 @@ namespace Lobby.Scripts
 {
     public class PaneRoomListing : MonoBehaviour
     {
-        private const string JOIN_PREFIX = "Join: ";
-
         [SerializeField] private Text title;
         [SerializeField] private Button templateButton;
         [SerializeField] private List<Button> buttons;
+
+        [SerializeField] private PhotonRoomList photonRoomList;
 
         private void Start()
         {
             title.text = $"Welcome to {Application.productName}";
             templateButton.onClick.AddListener(createRoomForMe);
             buttons = new List<Button>();
-            buttons.Add(templateButton);
-            buttons.Add(duplicate(templateButton, "TEST ROOM"));
+            photonRoomList = FindObjectOfType<PhotonRoomList>();
+        }
+
+        private void OnEnable()
+        {
+            if (PhotonNetwork.InLobby)
+            {
+                updateStatus();
+            }
+            if (photonRoomList != null)
+            {
+                photonRoomList.roomsUpdated += updateStatus;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (photonRoomList != null)
+            {
+                photonRoomList.roomsUpdated -= updateStatus;
+            }
+        }
+
+        private void updateStatus()
+        {
+            if (!PhotonNetwork.InLobby)
+            {
+                deleteExtraButtons(buttons, 0);
+                return;
+            }
+            var rooms = photonRoomList.currentRooms.ToList();
+            rooms.Sort((a, b) => string.Compare(a.Name, b.Name, StringComparison.Ordinal));
+            Debug.Log($"updateStatus enter {PhotonNetwork.NetworkClientState} buttons: {buttons.Count} rooms: {rooms.Count}");
+
+            // Synchronize button count with room count.
+            while (buttons.Count < rooms.Count)
+            {
+                buttons.Add(duplicate(templateButton));
+            }
+            deleteExtraButtons(buttons, rooms.Count);
+            // Update button captions
+            for (var i = 0; i < rooms.Count; ++i)
+            {
+                var room = rooms[i];
+                var button = buttons[i];
+                update(button, room.Name);
+            }
+            Debug.Log($"updateStatus exit {PhotonNetwork.NetworkClientState} buttons: {buttons.Count} rooms: {rooms.Count}");
         }
 
         private static void createRoomForMe()
         {
             Debug.Log("createRoomForMe");
+            PhotonLobby.Get().createRoom($"Room{DateTime.Now.Second:00}");
         }
 
         private static void joinRoom(string roomName)
@@ -31,15 +82,36 @@ namespace Lobby.Scripts
             Debug.Log($"joinRoom '{roomName}'");
         }
 
-        private static Button duplicate(Button template, string roomName)
+        private static Button duplicate(Button template)
         {
             var templateParent = template.gameObject;
             var instance = Instantiate(templateParent, templateParent.transform.parent);
             var button = instance.GetComponent<Button>();
-            var text = button.GetComponentInChildren<Text>();
-            text.text = $"{JOIN_PREFIX}{roomName}";
-            button.onClick.AddListener(() => joinRoom(roomName));
+            Debug.Log("duplicate");
             return button;
+        }
+
+        private static void update(Button button, string roomName)
+        {
+            var text = button.GetComponentInChildren<Text>();
+            Debug.Log($"update '{text.text}' -> '{roomName}'");
+            text.text = roomName;
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => joinRoom(roomName));
+        }
+
+        private static void deleteExtraButtons(List<Button> buttons, int buttonsToKeep)
+        {
+            while (buttons.Count > buttonsToKeep)
+            {
+                var lastButton = buttons[buttons.Count - 1];
+                if (!buttons.Remove(lastButton))
+                {
+                    throw new UnityException("can not remove button");
+                }
+                Debug.Log($"Destroy '{lastButton.GetComponentInChildren<Text>().text}'");
+                Destroy(lastButton.gameObject);
+            }
         }
     }
 }
