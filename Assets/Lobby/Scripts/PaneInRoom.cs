b3aa53baa12efdf02dfe0af6f813269bc26f1938
Jari Petays <jari.petays@iki.fi>
2021-09-23 15:11:39 +0300

    show player list for current room

--- Assets/Lobby/Scripts/PaneInRoom.cs
+++ Assets/Lobby/Scripts/PaneInRoom.cs
@@ -1,15 +1,20 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
 using Prg.Scripts.Common.PubSub;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Lobby.Scripts
 {
+    /// <summary>
+    /// Shows current player "state" in a room.
+    /// </summary>
     public class PaneInRoom : MonoBehaviour
     {
         [SerializeField] private Text title;
         [SerializeField] private Button[] buttons;
 
+        private ClientState curClientState;
         private void Start()
         {
             var room = PhotonNetwork.CurrentRoom;
@@ -17,5 +22,16 @@ namespace Lobby.Scripts
             buttons[0].onClick.AddListener(() => this.Publish(new LobbyManager.Event(LobbyManager.playerIsGuest)));
             buttons[1].onClick.AddListener(() => this.Publish(new LobbyManager.Event(LobbyManager.playerIsSpectator)));
         }
-   }
+
+        private void Update()
+        {
+            if (curClientState == PhotonNetwork.NetworkClientState)
+            {
+                return;
+            }
+            curClientState = PhotonNetwork.NetworkClientState;
+            var room = PhotonNetwork.CurrentRoom;
+            title.text = room?.Name ?? "Not in room";
+        }
+    }
 }
--- Assets/Lobby/Scripts/PanePlayerListing.cs
+++ Assets/Lobby/Scripts/PanePlayerListing.cs
@@ -1,19 +1,122 @@
-﻿using UnityEngine;
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Lobby.Scripts
 {
-    public class PanePlayerListing : MonoBehaviour
+    /// <summary>
+    /// Shows current players in a room as a list.
+    /// </summary>
+    public class PanePlayerListing : MonoBehaviour, IInRoomCallbacks
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private GameObject contentRoot;
+        [SerializeField] private Text textTemplate;
+
+        [SerializeField] private List<Text> textLines;
+
+        private void Start()
         {
-        
+            textLines = new List<Text>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnEnable()
         {
-        
+            if (PhotonNetwork.InRoom)
+            {
+                updateStatus();
+            }
+        }
+
+        private void updateStatus()
+        {
+            if (!PhotonNetwork.InRoom)
+            {
+                deleteExtraLines(textLines, 0);
+                return;
+            }
+            var players = PhotonNetwork.CurrentRoom.Players.Values.ToList();
+            players.Sort((a,b) => String.Compare(a.NickName, b.NickName, StringComparison.Ordinal));
+            Debug.Log($"updateStatus {PhotonNetwork.NetworkClientState} lines: {textLines.Count} players: {players.Count}");
+
+            // Synchronize line count with player count.
+            while (textLines.Count < players.Count)
+            {
+                textLines.Add(duplicate(textTemplate));
+            }
+            deleteExtraLines(textLines, players.Count);
+            // Update button captions
+            for (var i = 0; i < players.Count; ++i)
+            {
+                var player = players[i];
+                var line = textLines[i];
+                update(line, player);
+            }
+        }
+
+        private Text duplicate(Text template)
+        {
+            var templateParent = template.gameObject;
+            var instance = Instantiate(templateParent, contentRoot.transform);
+            instance.SetActive(true);
+            var text = instance.GetComponent<Text>();
+            Debug.Log("duplicate");
+            return text;
+        }
+
+        private void update(Text line, Player player)
+        {
+            var text = line.GetComponent<Text>();
+            var lineContent = player.IsLocal ? $"<b>{player.NickName}</b>" : player.NickName;
+            if (player.IsMasterClient)
+            {
+                lineContent += " [M]";
+            }
+            Debug.Log($"update '{text.text}' -> '{lineContent}'");
+            text.text = lineContent;
+        }
+
+        private static void deleteExtraLines(List<Text> lines, int linesToKeep)
+        {
+            while (lines.Count > linesToKeep)
+            {
+                var lastLine = lines[lines.Count - 1];
+                Debug.Log($"Destroy '{lastLine.GetComponent<Text>().text}'");
+                if (!lines.Remove(lastLine))
+                {
+                    throw new UnityException("can not remove line");
+                }
+                Destroy(lastLine.gameObject);
+            }
+        }
+
+        void IInRoomCallbacks.OnPlayerEnteredRoom(Player newPlayer)
+        {
+            updateStatus();
+        }
+
+        void IInRoomCallbacks.OnPlayerLeftRoom(Player otherPlayer)
+        {
+            updateStatus();
+        }
+
+        void IInRoomCallbacks.OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+        {
+            // NOP
+        }
+
+        void IInRoomCallbacks.OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+        {
+            // NOP
+        }
+
+        void IInRoomCallbacks.OnMasterClientSwitched(Player newMasterClient)
+        {
+            // NOP
         }
     }
 }
