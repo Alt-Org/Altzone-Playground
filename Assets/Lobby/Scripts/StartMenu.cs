37d24a6331720a4558dd4e4ff0c9938ab0fa813b
Jari Petays <jari.petays@iki.fi>
2021-09-24 14:51:09 +0300

    add simple (no-op) placeholder level for lobby etc. testing

---
+++ Assets/Lobby/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using UiProto.Scripts.Window;
+using UnityEngine;
+
+namespace Lobby.Scripts
+{
+    public class StartMenu : MonoBehaviour
+    {
+        [SerializeField] private LevelIdDef mainMenu;
+
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                SceneLoader.LoadScene(mainMenu.unityName);
+                enabled = false;
+            }
+        }
+    }
+}
