3c8ecf113a3cf30576fc4c5fd220975769905ca1
Jari Petays <jari.petays@iki.fi>
2021-09-25 12:16:45 +0300

    parse build report

--- Assets/Editor/Prg/Util/MenuBuildReport.cs
+++ Assets/Editor/Prg/Util/MenuBuildReport.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 namespace Editor.Prg.Util
 {
-    public class MenuBuildReport: MonoBehaviour
+    public class MenuBuildReport : MonoBehaviour
     {
         private static readonly string[] excludedFolders =
         {
@@ -24,16 +26,51 @@ namespace Editor.Prg.Util
                 return;
             }
 
-            var files = parseBuildReport(buildReport);
-            Debug.Log($"Build contains {files.Count} files");
+            var allFiles = parseBuildReport(buildReport, out var totalSize);
+            Debug.Log($"Build contains {allFiles.Count} asset files, total size is {totalSize:### ### ###.0} kb");
+            var assetFiles = new List<AssetLine>();
+            int[] fileCount = { 0, 0, 0};
+            float[] fileSize = { 0, 0, 0};
+            float[] filePercent = { 0, 0, 0};
+            foreach (var assetLine in allFiles)
+            {
+                if (assetLine.isAsset)
+                {
+                    assetFiles.Add(assetLine);
+                    fileCount[0] += 1;
+                    fileSize[0] += assetLine.fileSize;
+                    filePercent[0] += assetLine.percentage;
+                }
+                else if (assetLine.isPackage)
+                {
+                    fileCount[1] += 1;
+                    fileSize[1] += assetLine.fileSize;
+                    filePercent[1] += assetLine.percentage;
+                }
+                else if (assetLine.isResource)
+                {
+                    fileCount[2] += 1;
+                    fileSize[2] += assetLine.fileSize;
+                    filePercent[2] += assetLine.percentage;
+                }
+                else
+                {
+                    Debug.LogError("Unknown asset line: " + assetLine.filePath);
+                    return;
+                }
+            }
+            Debug.Log($"Build contains {fileCount[0]} ASSET files, their size is {fileSize[0]:### ### ###.0} kb ({filePercent[0]:0.0}%)");
+            Debug.Log($"Build contains {fileCount[1]} PACKAGE files, their size is {fileSize[1]:### ### ###.0} kb ({filePercent[1]:0.0}%)");
+            Debug.Log($"Build contains {fileCount[2]} RESOURCE files, their size is {fileSize[2]:### ### ###.0} kb ({filePercent[2]:0.0}%)");
         }
 
-        private static List<string> parseBuildReport(string buildReport)
+        private static List<AssetLine> parseBuildReport(string buildReport, out float totalSize)
         {
             const string markerLine = "-------------------------------------------------------------------------------";
             const string assetsLine = "Used Assets and files from the Resources folder, sorted by uncompressed size:";
-            var result = new List<string>();
+            var result = new List<AssetLine>();
             var processing = false;
+            totalSize = 0;
             foreach (var line in File.ReadAllLines(buildReport))
             {
                 if (processing)
@@ -42,7 +79,9 @@ namespace Editor.Prg.Util
                     {
                         break;
                     }
-                    result.Add(line);
+                    var assetLine = new AssetLine(line);
+                    totalSize += assetLine.fileSize;
+                    result.Add(assetLine);
                 }
                 if (line == assetsLine)
                 {
@@ -50,6 +89,39 @@ namespace Editor.Prg.Util
                 }
             }
             return result;
+        }
+
+        private class AssetLine
+        {
+            private static readonly CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+            private static readonly char[] separators1 = { '%' };
+            private static readonly char[] separators2 = { ' ' };
+
+            public readonly float fileSize;
+            public readonly float percentage;
+            public readonly string filePath;
+
+            public bool isAsset => filePath.StartsWith("Assets/");
+            public bool isPackage => filePath.StartsWith("Packages/");
+            public bool isResource => filePath.StartsWith("Resources/");
+
+            public AssetLine(string line)
+            {
+                var tokens = line.Split(separators1);
+                if (tokens.Length != 2)
+                {
+                    filePath = line;
+                    return;
+                }
+                filePath = tokens[1].Trim();
+                tokens = tokens[0].Split(separators2, StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length != 3)
+                {
+                    return;
+                }
+                fileSize = float.Parse(tokens[0], culture);
+                percentage = float.Parse(tokens[2], culture);
+            }
         }
     }
 }
