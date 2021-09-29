89fd8d263a3d028a4ed06fc495e927a632f9093f
Jari Petays <jari.petays@iki.fi>
2021-09-29 16:02:53 +0300

    add TriggerReturnToPool and attach it to mini ball

---
+++ Assets/Scripts/DigitalRuby/TriggerReturnToPool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace DigitalRuby
+{
+    public class TriggerReturnToPool : MonoBehaviour
+    {
+        public LayerMask collisionMask;
+        private int collisionLayer;
+
+        private void Awake()
+        {
+            collisionLayer = collisionMask.value;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            // var hasLayer = layerMask == (layerMask | 1 << _layer); // unity3d check if layer mask contains a layer
+
+            var hitObject = other.gameObject;
+            var hitLayer = hitObject.layer;
+            var colliderMask = 1 << hitLayer;
+            var hasLayer = collisionLayer == (collisionLayer | colliderMask);
+            if (hasLayer)
+            {
+                PoolManager.ReturnToCache(gameObject);
+            }
+        }
+    }
+}
