b229131a9572d38197c110a549dfa083d88da283
Jari Petays <jari.petays@iki.fi>
2021-10-03 14:39:18 +0300

    add more helper functions to clarify specific Photon states

--- Assets/Prg/Scripts/Common/Photon/PhotonWrapper.cs
+++ Assets/Prg/Scripts/Common/Photon/PhotonWrapper.cs
@@ -11,19 +11,42 @@ namespace Prg.Scripts.Common.Photon
 
         public static bool InLobby => PhotonNetwork.InLobby;
 
+        public static bool IsMasterClient => PhotonNetwork.IsMasterClient;
+
         /// <summary>
-        /// Can join lobby now?
+        /// Can join lobby if we are connected to a master server.
         /// </summary>
+        /// <remarks>
+        /// <c>NetworkClientState</c> is ConnectedToMasterServer.
+        /// </remarks>
+        public static bool CanJoinLobby => PhotonNetwork.NetworkClientState == ClientState.ConnectedToMasterServer;
+
+        /// <summary>
+        /// Can connect to Photon backend system with our settings if we are just created or disconnected.
+        /// </summary>
+        /// <remarks>
+        /// <c>NetworkClientState</c> is PeerCreated || Disconnected.
+        /// </remarks>
+        public static bool CanConnect => PhotonNetwork.NetworkClientState == ClientState.PeerCreated ||
+                                         PhotonNetwork.NetworkClientState == ClientState.Disconnected;
+
+        /// <summary>
+        /// Photon network is idle or on master server.
+        /// </summary>
+        /// <remarks>
+        /// <c>NetworkClientState</c> is PeerCreated || Disconnected || ConnectedToMasterServer.
+        /// </remarks>
         public static bool IsPhotonReady => isPhotonReady();
 
         public static void LoadLevel(string levelUnityName)
         {
             PhotonNetwork.LoadLevel(levelUnityName);
         }
+
         private static bool isPhotonReady()
         {
             var state = PhotonNetwork.NetworkClientState;
-            return state == ClientState.PeerCreated || state == ClientState.ConnectedToMasterServer || state == ClientState.Disconnected;
+            return state == ClientState.PeerCreated || state == ClientState.Disconnected || state == ClientState.ConnectedToMasterServer;
         }
     }
 }
