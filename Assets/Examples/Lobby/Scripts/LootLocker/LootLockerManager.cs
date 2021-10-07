49d76c6ac99b332822470efef98e7417da88a3fe
Jari Petays <jari.petays@iki.fi>
2021-10-07 14:48:07 +0300

    add LootLocker with test scene and script (connect does not work)

---
+++ Assets/Examples/Lobby/Scripts/LootLocker/LootLockerManager.cs
@@ -0,0 +1,23 @@
+﻿using LootLocker.Requests;
+using UnityEngine;
+
+namespace Examples.Lobby.Scripts.LootLocker
+{
+    public class LootLockerManager : MonoBehaviour
+    {
+        public string playerIdentifier;
+
+        private void Start()
+        {
+            Debug.Log($"StartSession for {playerIdentifier}");
+            LootLockerSDKManager.StartSession(playerIdentifier, (response) =>
+            {
+                Debug.Log($"StartSession: {response.success}");
+                if (!response.success)
+                {
+                    Debug.Log($"{response.statusCode} {response.Error.Replace("\n    ", " ")}");
+                }
+            });
+        }
+    }
+}
