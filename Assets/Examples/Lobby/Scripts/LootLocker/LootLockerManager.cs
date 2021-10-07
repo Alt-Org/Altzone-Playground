706ce603cfc33293b2580583286ad1f1a1e51493
Jari Petays <jari.petays@iki.fi>
2021-10-07 18:03:03 +0300

    LootLocker session can be established OK but code has no meaningful logic yet!

--- Assets/Examples/Lobby/Scripts/LootLocker/LootLockerManager.cs
+++ Assets/Examples/Lobby/Scripts/LootLocker/LootLockerManager.cs
@@ -1,18 +1,44 @@
-﻿using LootLocker.Requests;
+﻿using LootLocker;
+using LootLocker.Requests;
+using System;
 using UnityEngine;
 
 namespace Examples.Lobby.Scripts.LootLocker
 {
+    /// <summary>
+    /// Test driver for LootLocker Game BAAS
+    /// </summary>
     public class LootLockerManager : MonoBehaviour
     {
-        public string playerIdentifier;
+        public void VerifyID(string playerIdentifier, Action<bool> onComplete)
+        {
+            var config = LootLockerConfig.Get();
+            if (!(config.platform == LootLockerConfig.platformType.Steam || config.platform == LootLockerConfig.platformType.PlayStationNetwork))
+            {
+                throw new UnityException($"DO NOT CALL VerifyID for {playerIdentifier} - it is reserved for STEAM or PLAYSTATION platforms ONLY!");
+            }
+            Debug.Log($"VerifyID for {playerIdentifier}");
+            LootLockerSDKManager.VerifyID(playerIdentifier, response =>
+            {
+                Debug.Log($"VerifyID: {response.success}");
+                if (response.success)
+                {
+                    Debug.Log($"{response.statusCode} {response.text.Replace("\n    ", " ")}");
+                }
+                else
+                {
+                    Debug.Log($"{response.statusCode} {response.Error.Replace("\n    ", " ")}");
+                }
+                onComplete?.Invoke(response.success);
+            });
+        }
 
-        private void Start()
+        public void StartSession(string playerIdentifier, Action<bool> onComplete)
         {
             Debug.Log($"StartSession for {playerIdentifier}");
             LootLockerSDKManager.StartSession(playerIdentifier, (response) =>
             {
-                Debug.Log($"StartSession: {response.success}");
+                Debug.Log($"StartSession for {playerIdentifier}: {response.success}");
                 if (response.success)
                 {
                     Debug.Log($"{response.statusCode} {response.text.Replace("\n    ", " ")}");
