8157e5e0ae5fbea0af88c399580cedce75fec201
Jari Petays <jari.petays@iki.fi>
2021-10-09 09:20:55 +0300

    improve LootLockerAsync code and comments

--- Assets/Examples/Lobby/Scripts/LootLocker/LootLockerAsync.cs
+++ Assets/Examples/Lobby/Scripts/LootLocker/LootLockerAsync.cs
@@ -6,36 +6,39 @@ namespace Examples.Lobby.Scripts.LootLocker
     /// <summary>
     /// Async wrapper to <c>LootLocker</c> SDK API.
     /// </summary>
+    /// <remarks>
+    /// We use <c>TaskCompletionSource</c> to bind caller and callee "together".
+    /// </remarks>
     public static class LootLockerAsync
     {
-        public static async Task<LootLockerSessionResponse> StartSession(string deviceId)
+        public static Task<LootLockerSessionResponse> StartSession(string deviceId)
         {
             var taskCompletionSource = new TaskCompletionSource<LootLockerSessionResponse>();
             LootLockerSDKManager.StartSession(deviceId, response =>
             {
-                taskCompletionSource.TrySetResult(response);
+                taskCompletionSource.SetResult(response);
             });
-            return await taskCompletionSource.Task;
+            return taskCompletionSource.Task;
         }
 
-        public static async Task<PlayerNameResponse> SetPlayerName(string playerName)
+        public static Task<PlayerNameResponse> SetPlayerName(string playerName)
         {
             var taskCompletionSource = new TaskCompletionSource<PlayerNameResponse>();
             LootLockerSDKManager.SetPlayerName(playerName, response =>
             {
-                taskCompletionSource.TrySetResult(response);
+                taskCompletionSource.SetResult(response);
             });
-            return await taskCompletionSource.Task;
+            return taskCompletionSource.Task;
         }
 
-        public static async Task<PlayerNameResponse> GetPlayerName()
+        public static Task<PlayerNameResponse> GetPlayerName()
         {
             var taskCompletionSource = new TaskCompletionSource<PlayerNameResponse>();
             LootLockerSDKManager.GetPlayerName(response =>
             {
-                taskCompletionSource.TrySetResult(response);
+                taskCompletionSource.SetResult(response);
             });
-            return await taskCompletionSource.Task;
+            return taskCompletionSource.Task;
         }
     }
 }
--- Assets/Examples/Lobby/Scripts/LootLocker/LootLockerManager.cs
+++ Assets/Examples/Lobby/Scripts/LootLocker/LootLockerManager.cs
@@ -1,5 +1,6 @@
 ﻿using LootLocker.Requests;
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Examples.Lobby.Scripts.LootLocker
@@ -41,84 +42,72 @@ namespace Examples.Lobby.Scripts.LootLocker
     /// </remarks>
     public class LootLockerManager : MonoBehaviour
     {
+        [SerializeField] private bool isAsyncMode;
         [SerializeField] private PlayerHandle _playerHandle;
         [SerializeField] private bool isStartSessionReady;
 
         public PlayerHandle playerHandle => _playerHandle;
         public bool isValid => _playerHandle != null && isStartSessionReady;
 
+        /// <summary>
+        /// Asynchronous methods can be mind-boggling even though they make life a lot easier!
+        /// </summary>
         private async void Awake()
         {
+            Debug.Log("Awake start");
             isStartSessionReady = false;
-            var deviceId = PlayerPrefs.GetString("lootLocker.deviceId", "");
-            if (string.IsNullOrWhiteSpace(deviceId))
+            if (isAsyncMode)
             {
-                deviceId = Guid.NewGuid().ToString();
-                PlayerPrefs.SetString("lootLocker.deviceId", deviceId);
+                await asyncInit();
+                isStartSessionReady = true;
             }
-            var playerName = PlayerPrefs.GetString("lootLocker.playerName", "");
-            if (string.IsNullOrWhiteSpace(playerName))
+            else
             {
-                playerName = $"Player{1000 * (1 + DateTime.Now.Second % 10) + DateTime.Now.Millisecond:00}";
-                PlayerPrefs.SetString("lootLocker.playerName", playerName);
+                callbackInit();
             }
-            try
+            Debug.Log($"Awake exit {isStartSessionReady}");
+        }
+
+        private async Task asyncInit()
+        {
+            getPlayerPrefs(out var deviceId, out var playerName);
+            Debug.Log($"StartSession for {playerName} {deviceId}");
+            var sessionResp = await LootLockerAsync.StartSession(deviceId);
+            if (!sessionResp.success)
             {
-                Debug.Log($"StartSession for {playerName} {deviceId}");
-                var sessionResp = await LootLockerAsync.StartSession(deviceId);
-                if (!sessionResp.success)
-                {
-                    // Create dummy player using PlayerPrefs values
-                    _playerHandle = new PlayerHandle(deviceId, playerName, 0, "", "");
-                    return;
-                }
-                _playerHandle = new PlayerHandle(deviceId, playerName, sessionResp.player_id, sessionResp.public_uid, sessionResp.session_token);
+                // Create dummy player using PlayerPrefs values
+                _playerHandle = new PlayerHandle(deviceId, playerName, 0, "", "");
+                return;
+            }
+            _playerHandle = new PlayerHandle(deviceId, playerName, sessionResp.player_id, sessionResp.public_uid, sessionResp.session_token);
 
-                if (!sessionResp.seen_before)
-                {
-                    // This is new player
-                    Debug.Log($"SetPlayerName is NEW '{_playerHandle.PlayerName}'");
-                    LootLockerSDKManager.SetPlayerName(_playerHandle.PlayerName, null); // Fire and forget
-                    return;
-                }
+            if (!sessionResp.seen_before)
+            {
+                // This is new player
+                Debug.Log($"SetPlayerName is NEW '{_playerHandle.PlayerName}'");
+                var task = LootLockerAsync.SetPlayerName(_playerHandle.PlayerName); // Fire and forget
+                return;
+            }
 
-                var getNameResp = await LootLockerAsync.GetPlayerName();
-                if (!getNameResp.success || string.IsNullOrWhiteSpace(getNameResp.name))
-                {
-                    // Failed to get or name is empty
-                    Debug.Log($"SetPlayerName '{_playerHandle.PlayerName}'");
-                    LootLockerSDKManager.SetPlayerName(_playerHandle.PlayerName, null); // Fire and forget
-                    return;
-                }
-                if (_playerHandle.PlayerName == getNameResp.name)
-                {
-                    return; // We are synchronized
-                }
+            var getNameResp = await LootLockerAsync.GetPlayerName();
+            if (!getNameResp.success || string.IsNullOrWhiteSpace(getNameResp.name))
+            {
+                // Failed to get or name is empty
+                Debug.Log($"SetPlayerName '{_playerHandle.PlayerName}'");
+                var task = LootLockerAsync.SetPlayerName(_playerHandle.PlayerName); // Fire and forget
+                return;
+            }
+            if (_playerHandle.PlayerName != getNameResp.name)
+            {
                 // Update local name from LootLocker
                 _playerHandle.PlayerName = getNameResp.name;
                 PlayerPrefs.SetString("lootLocker.playerName", _playerHandle.PlayerName);
-            }
-            finally
-            {
-                isStartSessionReady = true;
             }
         }
 
         private void callbackInit()
         {
-            var deviceId = PlayerPrefs.GetString("lootLocker.deviceId", "");
-            if (string.IsNullOrWhiteSpace(deviceId))
-            {
-                deviceId = Guid.NewGuid().ToString();
-                PlayerPrefs.SetString("lootLocker.deviceId", deviceId);
-            }
-            var playerName = PlayerPrefs.GetString("lootLocker.playerName", "");
-            if (string.IsNullOrWhiteSpace(playerName))
-            {
-                playerName = $"Player{1000 * (1 + DateTime.Now.Second % 10) + DateTime.Now.Millisecond:00}";
-                PlayerPrefs.SetString("lootLocker.playerName", playerName);
-            }
-            isStartSessionReady = false;
+            getPlayerPrefs(out var deviceId, out var playerName);
             Debug.Log($"StartSession for {playerName} {deviceId}");
             LootLockerSDKManager.StartSession(deviceId, (sessionResp) =>
             {
@@ -168,6 +157,22 @@ namespace Examples.Lobby.Scripts.LootLocker
                     });
                 });
             });
+        }
+
+        private static void getPlayerPrefs(out string deviceId, out string playerName)
+        {
+            deviceId = PlayerPrefs.GetString("lootLocker.deviceId", "");
+            if (string.IsNullOrWhiteSpace(deviceId))
+            {
+                deviceId = Guid.NewGuid().ToString();
+                PlayerPrefs.SetString("lootLocker.deviceId", deviceId);
+            }
+            playerName = PlayerPrefs.GetString("lootLocker.playerName", "");
+            if (string.IsNullOrWhiteSpace(playerName))
+            {
+                playerName = $"Player{1000 * (1 + DateTime.Now.Second % 10) + DateTime.Now.Millisecond:00}";
+                PlayerPrefs.SetString("lootLocker.playerName", playerName);
+            }
         }
 
         private bool isApplicationQuit;
