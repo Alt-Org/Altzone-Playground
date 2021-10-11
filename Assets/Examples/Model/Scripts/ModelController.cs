eace27d5f2cd008f266110625e3f613552a98b13
Jari Petays <jari.petays@iki.fi>
2021-10-11 10:50:35 +0300

    improve responsibilities between view and controller

--- Assets/Examples/Model/Scripts/ModelController.cs
+++ Assets/Examples/Model/Scripts/ModelController.cs
@@ -22,6 +22,7 @@ namespace Examples.Model.Scripts
             view.titleText.text = $"<b>Choose your character</b>\r\nfor {Application.productName} {PhotonLobby.gameVersion}";
             var player = RuntimeGameConfig.Get().playerDataCache;
             view.playerName.text = player.PlayerName;
+            view.hideCharacter();
             view.continueButton.onClick.AddListener(() =>
             {
                 // Save player settings if changed before continuing!
@@ -37,36 +38,23 @@ namespace Examples.Model.Scripts
                 manager.Continue();
             });
             currentCharacterId = player.CharacterModelId;
-            var buttons = view.getButtons();
             var characters = Models.GetAll<CharacterModel>();
             characters.Sort((a, b) => string.Compare(a.sortValue(), b.sortValue(), StringComparison.Ordinal));
             for (var i = 0; i < characters.Count; ++i)
             {
-                var button = buttons[i];
                 var character = characters[i];
+                var button = view.getButton(i);
                 button.SetCaption(character.Name);
                 button.onClick.AddListener(() =>
                 {
                     currentCharacterId = character.Id;
-                    showCharacter(character);
+                    view.showCharacter(character);
                 });
                 if (currentCharacterId == character.Id)
                 {
-                    showCharacter(character);
+                    view.showCharacter(character);
                 }
             }
-        }
-
-        private void showCharacter(CharacterModel character)
-        {
-            var labels = view.getTextLabels();
-            var i = -1;
-            labels[++i].text = $"<b>{character.Name}</b>";
-            labels[++i].text = $"MainDefence:\r\n{character.MainDefence}";
-            labels[++i].text = $"Speed:\r\n{character.Speed}";
-            labels[++i].text = $"Resistance:\r\n{character.Resistance}";
-            labels[++i].text = $"Attack:\r\n{character.Attack}";
-            labels[++i].text = $"Defence:\r\n{character.Defence}";
         }
     }
 }
--- Assets/Examples/Model/Scripts/ModelView.cs
+++ Assets/Examples/Model/Scripts/ModelView.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using Examples.Model.Scripts.Model;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,14 +16,37 @@ namespace Examples.Model.Scripts
         [SerializeField] private Transform leftPane;
         [SerializeField] private Transform rightPane;
 
-        public List<Button> getButtons()
+        private Button[] buttons;
+        private Text[] labels;
+
+        private void Awake()
         {
-            return leftPane.GetComponentsInChildren<Button>().ToList();
+            buttons = leftPane.GetComponentsInChildren<Button>();
+            labels = rightPane.GetComponentsInChildren<Text>();
         }
 
-        public List<Text> getTextLabels()
+        public Button getButton(int buttonIndex)
         {
-            return rightPane.GetComponentsInChildren<Text>().ToList();
+            return buttons[buttonIndex];
+        }
+
+        public void hideCharacter()
+        {
+            foreach (var label in labels)
+            {
+                label.text = "";
+            }
+        }
+
+        public void showCharacter(CharacterModel character)
+        {
+            var i = -1;
+            labels[++i].text = $"<b>{character.Name}</b>";
+            labels[++i].text = $"MainDefence:\r\n{character.MainDefence}";
+            labels[++i].text = $"Speed:\r\n{character.Speed}";
+            labels[++i].text = $"Resistance:\r\n{character.Resistance}";
+            labels[++i].text = $"Attack:\r\n{character.Attack}";
+            labels[++i].text = $"Defence:\r\n{character.Defence}";
         }
     }
 }
