6e8590a3b55d7a21810b9fa3bacb315089fed0b3
Jari Petays <jari.petays@iki.fi>
2021-10-14 14:04:25 +0300

    finalize ScoreFlash

--- Assets/Prg/Scripts/Common/Unity/ScoreFlash/ScoreFlash.cs
+++ Assets/Prg/Scripts/Common/Unity/ScoreFlash/ScoreFlash.cs
@@ -6,22 +6,17 @@ namespace Prg.Scripts.Common.Unity.ScoreFlash
 {
     public class ScoreFlash : MonoBehaviour
     {
-        public string displayTextFormat;
-        public float countDownTime;
-        [SerializeField] private TMP_Text textMeshPro;
+        [SerializeField] private float countDownTime;
+        [SerializeField] private Vector3 punchForce;
+        [SerializeField] private GameObject parent;
+        [SerializeField] private TMP_Text tmpText;
 
-        private GameObject parent;
-        private Transform parentTransform;
-        private Vector3 endPosition;
+        private Transform textTransform;
 
         private void OnEnable()
         {
-            parent = textMeshPro.gameObject;
-            parentTransform = parent.transform;
+            textTransform = tmpText.transform;
             parent.SetActive(true);
-
-            Debug.Log($"start countDownTime={countDownTime:0.0}");
-            endPosition = Vector3.one;
             move();
         }
 
@@ -29,14 +24,14 @@ namespace Prg.Scripts.Common.Unity.ScoreFlash
         {
             // http://dotween.demigiant.com/documentation.php
 
-            parentTransform.DOPunchScale(endPosition, 1f).OnComplete(() =>
+            textTransform.DOPunchScale(punchForce, 1f).OnComplete(() =>
             {
                 countDownTime -= 1f;
-                textMeshPro.text = string.Format(displayTextFormat, countDownTime);
-                Debug.Log($"start countDownTime={countDownTime:0.0}");
+                tmpText.text = countDownTime.ToString("N0");
                 if (countDownTime < 0)
                 {
                     parent.SetActive(false);
+                    gameObject.SetActive(false);
                 }
                 else
                 {
