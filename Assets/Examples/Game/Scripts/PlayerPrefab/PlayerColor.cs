8996fdc3bf461b218dcafbd8dda309279937b0c9
Jari Petays <jari.petays@iki.fi>
2021-10-14 19:44:51 +0300

    synchronize player color over network. actually this is not enough as collider state is not reflected here! we need a solution where local player state is atomically sent to all other (remote) players

--- Assets/Examples/Game/Scripts/PlayerPrefab/PlayerColor.cs
+++ Assets/Examples/Game/Scripts/PlayerPrefab/PlayerColor.cs
@@ -1,31 +1,84 @@
+using Prg.Scripts.Common.Photon;
 using UnityEngine;
 
 namespace Examples.Game.Scripts.PlayerPrefab
 {
     /// <summary>
-    /// Manages some player colors during a game.
+    /// Manages some debug player colors during a game over network.
     /// </summary>
     public class PlayerColor : MonoBehaviour
     {
+        private const int photonEventCode = PhotonEventDispatcher.eventCodeBase + 6;
+
         [Header("Settings"), SerializeField] private SpriteRenderer headSprite;
-        [SerializeField] private Color normalColor;
-        [SerializeField] private Color disabledColor;
-        [SerializeField] private Color ghostColor;
+        [SerializeField] private Color normalColor; // #0
+        [SerializeField] private Color disabledColor; // #1
+        [SerializeField] private Color ghostColor; // #2
         [SerializeField] private SpriteRenderer _highLightSprite;
+
+        private PhotonEventDispatcher photonEventDispatcher;
+
+        private void Awake()
+        {
+            photonEventDispatcher = PhotonEventDispatcher.Get();
+            photonEventDispatcher.registerEventListener(photonEventCode, data => receiveColorIndex(data.CustomData));
+        }
+
+        private void receiveColorIndex(object data)
+        {
+            var colorIndex = (int)data;
+            switch (colorIndex)
+            {
+                case 0:
+                    _setNormalColor();
+                    break;
+                case 1:
+                    _setDisabledColor();
+                    break;
+                case 2:
+                    _setGhostColor();
+                    break;
+                default:
+                    throw new UnityException($"invalid color index: {colorIndex}");
+            }
+        }
+
+        private void sendColorIndex(int index)
+        {
+            photonEventDispatcher.RaiseEvent(photonEventCode, index);
+        }
 
         public void setNormalColor()
         {
-            headSprite.color = normalColor;
-        }
-
-        public void setGhostColor()
-        {
-            headSprite.color = ghostColor;
+            sendColorIndex(0);
+            _setNormalColor();
         }
 
         public void setDisabledColor()
         {
+            sendColorIndex(1);
+            _setDisabledColor();
+        }
+
+        public void setGhostColor()
+        {
+            sendColorIndex(2);
+            _setGhostColor();
+        }
+
+        private void _setNormalColor()
+        {
+            headSprite.color = normalColor;
+        }
+
+        private void _setDisabledColor()
+        {
             headSprite.color = disabledColor;
+        }
+
+        private void _setGhostColor()
+        {
+            headSprite.color = ghostColor;
         }
 
         public void setHighLightColor(Color color)
