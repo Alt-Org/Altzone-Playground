54a7d7696f79ef00067484f6b0d844eef135efdc
Jari Petays <jari.petays@iki.fi>
2021-10-15 16:05:54 +0300

    restart examples

---
+++ Assets/Examples/Game/Scripts/Battle/Ball/BallActor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Examples.Game.Scripts.Battle.Ball
+{
+    public class BallActor : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("Awake");
+
+            enabled = false; // Wait until game starts
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log("OnEnable");
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log("OnDisable");
+        }
+    }
+}
---
+++ Assets/Examples/Game/Scripts/Battle/Player/PlayerActor.cs
@@ -0,0 +1,40 @@
+﻿using Examples.Config.Scripts;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Examples.Game.Scripts.Battle.Player
+{
+    public class PlayerActor : MonoBehaviour
+    {
+        [Header("Settings"), SerializeField] private GameObject[] shields;
+
+        [Header("Live Data"), SerializeField] private int playerPos;
+        [SerializeField] private int teamIndex;
+        [SerializeField] private Collider2D[] colliders;
+
+        public int PlayerPos => playerPos;
+        public int TeamIndex => teamIndex;
+
+        private void Awake()
+        {
+            var player = PhotonNetwork.LocalPlayer;
+            PhotonBattle.getPlayerProperties(player, out playerPos, out teamIndex);
+            Debug.Log($"Awake {player.NickName} pos={playerPos} team={teamIndex}");
+            var oppositeTeam = teamIndex == 0 ? 1 : 0;
+            shields[oppositeTeam].SetActive(false);
+            colliders = GetComponentsInChildren<Collider2D>(includeInactive: false);
+
+            enabled = false; // Wait until game starts
+        }
+
+        private void OnEnable()
+        {
+            Debug.Log($"OnEnable pos={playerPos} team={teamIndex}");
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log($"OnDisable pos={playerPos} team={teamIndex}");
+        }
+    }
+}
