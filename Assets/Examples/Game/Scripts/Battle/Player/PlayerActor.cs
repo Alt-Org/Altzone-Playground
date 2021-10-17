e28bb9d4c2442bd0c15c5d632cba94b57a5626bf
Jari Petays <jari.petays@iki.fi>
2021-10-17 15:07:13 +0300

    add getTeamMatePos

--- Assets/Examples/Game/Scripts/Battle/Player/PlayerActor.cs
+++ Assets/Examples/Game/Scripts/Battle/Player/PlayerActor.cs
@@ -13,6 +13,7 @@ namespace Examples.Game.Scripts.Battle.Player
         [SerializeField] private Collider2D[] colliders;
 
         public int PlayerPos => playerPos;
+        public int TeamMatePos => getTeamMatePos(playerPos);
         public int TeamIndex => teamIndex;
 
         private void Awake()
@@ -35,6 +36,23 @@ namespace Examples.Game.Scripts.Battle.Player
         private void OnDisable()
         {
             Debug.Log($"OnDisable pos={playerPos} team={teamIndex}");
+        }
+
+        private static int getTeamMatePos(int playerPos)
+        {
+            switch (playerPos)
+            {
+                case 0:
+                    return 2;
+                case 1:
+                    return 3;
+                case 2:
+                    return 0;
+                case 3:
+                    return 1;
+                default:
+                    throw new UnityException($"invalid player pos: {playerPos}");
+            }
         }
     }
 }
