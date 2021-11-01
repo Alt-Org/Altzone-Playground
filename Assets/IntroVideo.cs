351d264ee4e8b56b162a07413e1a3752f8f33562
Mikko <juha1838@student.hamk.fi>
2021-11-01 21:10:28 +0200

    Intron kilpiversioon konvertointi. Videoscript.
    Intron kilpiversioon konvertointi. Tekstien muutokset. Videoscript.

---
+++ Assets/IntroVideo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Playables;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class IntroVideo : MonoBehaviour
+{
+    //public GameObject loadingImage;
+
+    public GameObject IntroStart; 
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += EndReached;
+    }
+            /*
+    IEnumerator playVideo()
+    {
+
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            loadingImage.SetActive(true);
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+        loadingImage.SetActive(false);
+
+        videoPlayer.Play();
+    }
+            */
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        Destroy(gameObject);
+        IntroStart.SetActive(true);
+    }
+}
