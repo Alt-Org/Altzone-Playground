d50906e2c939fab853beba89986e1d1f3c201b2b
Mikko <juha1838@student.hamk.fi>
2022-03-11 00:10:52 +0200

    Park shield pick based camera. mp4 removed again
    Park shield pick based camera. .mp4 file removed again after revert

--- Assets/Park/Scripts/CameraTransform.cs
+++ Assets/Park/Scripts/CameraTransform.cs
@@ -7,29 +7,30 @@ public class CameraTransform : MonoBehaviour
     public Transform targetTransform;
     public Vector3 position;
     public Vector3 rotation;
-    public bool useWorldSpace = true;
     public float tweenDuration = 1.0f;
+    public bool useWorldSpace = true;
+    private Coroutine currentCoroutine = null;
 
-    //private Coroutine _currentCoroutine = null;
 
-
-    /*
-    void Start()
+    public void TweenToRotation()
     {
-        
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+            currentCoroutine = null;
+        }
+
+        if (gameObject.activeInHierarchy && targetTransform != null)
+        {
+            Quaternion targetRotation = Quaternion.Euler(rotation);
+            currentCoroutine = StartCoroutine(TweenRotate(tweenDuration, targetRotation, useWorldSpace));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    */
 
-    private IEnumerator TweenMoveRotate(float duration, Transform target)
+    private IEnumerator TweenRotate(float duration, Quaternion targetRotation, bool worldSpace)
     {
-        Vector3 startPosition = targetTransform.position;
-        Quaternion startRotation = targetTransform.rotation;
+        Quaternion startRotation = worldSpace ? targetTransform.rotation : targetTransform.localRotation;
 
         float t = 0;
         float timeElapsed = 0;
@@ -39,15 +40,31 @@ public class CameraTransform : MonoBehaviour
             timeElapsed += Time.deltaTime;
             t = timeElapsed / duration;
 
-            targetTransform.position = Vector3.Lerp(startPosition, target.position, t);
-            targetTransform.rotation = Quaternion.Lerp(startRotation, target.rotation, t);
+            if (worldSpace)
+            {
+                targetTransform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            }
+            else
+            {
+                targetTransform.localRotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            }
             yield return null;
         }
-        targetTransform.position = target.position;
-        targetTransform.rotation = target.rotation;
 
-
-
+        if (worldSpace)
+        {
+            targetTransform.rotation = targetRotation;
+        }
+        else
+        {
+            targetTransform.localRotation = targetRotation;
+        }
     }
 
 }
+
+
+
+
+
+
